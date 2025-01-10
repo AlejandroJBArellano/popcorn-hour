95e0fffccf477b2bce9c4655dd892a4b673aafb1
Alejandro Arellano <jesusarellano334@gmail.com>
2025-01-10 07:15:57 -0600

    Chore: Renombrar variable de Blueprint en el módulo de reseñas

--- app/routes/review.py
+++ app/routes/review.py
@@ -2,19 +2,19 @@ from flask import Blueprint, request, jsonify
 from app.models.review import Review
 from app import db
 
-review_bp = Blueprint('review', __name__)
+bp = Blueprint('review', __name__)
 
-@review_bp.route('/reviews', methods=['GET'])
+@bp.route('/reviews', methods=['GET'])
 def get_reviews():
     reviews = Review.query.all()
     return jsonify([review.to_dict() for review in reviews])
 
-@review_bp.route('/reviews/<int:id>', methods=['GET'])
+@bp.route('/reviews/<int:id>', methods=['GET'])
 def get_review(id):
     review = Review.query.get_or_404(id)
     return jsonify(review.to_dict())
 
-@review_bp.route('/reviews', methods=['POST'])
+@bp.route('/reviews', methods=['POST'])
 def create_review():
     data = request.get_json()
     new_review = Review(
@@ -27,7 +27,7 @@ def create_review():
     db.session.commit()
     return jsonify(new_review.to_dict()), 201
 
-@review_bp.route('/reviews/<int:id>', methods=['PUT'])
+@bp.route('/reviews/<int:id>', methods=['PUT'])
 def update_review(id):
     review = Review.query.get_or_404(id)
     data = request.get_json()
@@ -36,7 +36,7 @@ def update_review(id):
     db.session.commit()
     return jsonify(review.to_dict())
 
-@review_bp.route('/reviews/<int:id>', methods=['DELETE'])
+@bp.route('/reviews/<int:id>', methods=['DELETE'])
 def delete_review(id):
     review = Review.query.get_or_404(id)
     db.session.delete(review)
